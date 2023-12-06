7792f9f363efeb21bf6e1dcb70d5fc638c0ead3f
RGHmeu <149521969+RGHmeu@users.noreply.github.com>
2023-12-06 10:49:17 -0300

    Add files via upload

---
+++ schemas/__init__.py
@@ -0,0 +1,4 @@
+from schemas.movimento import MovimentoSchema, MovimentoBuscaSchema, MovimentoViewSchema, \
+                            ListagemMovimentosSchema, MovimentoDelSchema, \
+                            apresenta_movimento, apresenta_movimentos
+from schemas.error import ErrorSchema
